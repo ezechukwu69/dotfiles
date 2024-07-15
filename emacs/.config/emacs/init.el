843d58e20b044716a6f8d43c0654565b2e3c2d3e
Ezechukwu Ojukwu <ezechukwu69@gmail.com>
2024-07-15 05:09:18 +0100

    Added all dotfiles for backup

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,4 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  "~/.config/emacs/"))
---
+++ qtile/.config/qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nm-applet &
+blueman-applet &
+dunst &
+xfce4-power-manager &
+picom --corner-radius 12 --transparent-clipping --blur-size 0 --shadow-exclude 'bounding_shaped && !rounded_corners' --shadow-opacity 0 --no-fading-openclose &
+xrandr --output DP-1 --auto --right-of HDMI-3
+nitrogen --restore &
+~/.config/i3/launch_polybar.sh &
