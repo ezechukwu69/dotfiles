74d26fb49862217397a05b47abb0ce31325ac214
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2024-11-18 00:01:46 +0100

    Update to tasks

--- hypr2/.config/hypr/run-hyprsunset.sh
+++ hypr2/.config/hypr/run-hyprsunset.sh
@@ -8,9 +8,11 @@ echo "PID: $pid"
 if [ -n "$pid" ]; then
   # If it's running, kill it
   echo "Terminating hyprsunset process with PID $pid..."
+  notify-send "Turning off hyprsunset..."
   kill "$pid"
 else
   # If it's not running, start it
   echo "Starting hyprsunset..."
+  notify-send "Turning on hyprsunset..."
   hyprsunset &
 fi
