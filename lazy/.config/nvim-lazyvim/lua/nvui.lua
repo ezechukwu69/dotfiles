6d61b6c7fce6eec363b086a81ed428d4b3bf1c8c
Ezechukwu Ojukwu <ezechukwu69@gmail.com>
2024-12-29 17:35:26 +0100

    Update to nvim-remote

--- lazy/.config/nvim-lazyvim/lua/nvui.lua
+++ lazy/.config/nvim-lazyvim/lua/nvui.lua
@@ -10,7 +10,7 @@ return {
   {
     "nvim-tree/nvim-web-devicons",
     opts = function()
-      dofile(vim.g.base46_cache .. "devicons")
+      -- dofile(vim.g.base46_cache .. "devicons")
       return { override = require("nvchad.icons.devicons") }
     end,
   },
