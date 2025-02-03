b90bc6e205b04582459a4e9e854ce02be58180a7
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2025-02-03 10:15:32 +0100

    Update to home manager

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -129,8 +129,8 @@ export PATH="$HOME/.config/nvim/bin:$PATH"
 export PATH="$HOME/.cache/lm-studio/bin:$PATH"
 export PATH="$HOME/.local/share/gem/ruby/3.3.0/bin:$PATH"
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-eval $(ssh-agent) > /dev/null
-ssh-add ~/.ssh/id_ed25519 &> /dev/null
+eval $(ssh-agent) 2> /dev/null
+ssh-add ~/.ssh/id_ed25519 2> /dev/null
 alias v="lvim"
 alias et="emacs -nw"
 alias e="emacs"
@@ -145,7 +145,7 @@ else
   touch ~/.zshrc_local
   source ~/.zshrc_local
 fi
-bindkey -o
+bindkey -v
 fastfetch
     '';
   };
