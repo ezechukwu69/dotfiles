fcbe59cfd0b04bb36dff8969d6c9f65b55ecbf92
ezechukwu69 <ezechukwu69@gmail.com>
2025-06-29 11:06:53 +0100

    Convert to plugin structure supporting setup function

--- custom-nvim/.config/nvim-custom/lua/config/jj.lua
+++ custom-nvim/.config/nvim-custom/lua/config/jj.lua
@@ -1,9 +1,11 @@
 local M = {
-    width = 0.4,
-    height = 0.9,
-    float_buf = nil,
-    float_win = nil,
-
+    opts = {
+        width = 0.4,
+        height = 0.9,
+        float_buf = nil,
+        float_win = nil,
+        set_default_keymaps = true,
+    },
     log =
     "jj --no-pager --color never --no-graph -T \"change_id.short()  ++ '	 (' ++ committer.name() ++ ') 	 ' ++ description.first_line() ++ bookmarks.map(|item| ' 	*' ++ item.name() ++ if(item.remote(), '@' ++ item.remote())).join(' ') ++ ' \n'\"",
 
@@ -11,49 +13,50 @@ local M = {
 
 function M.open_float_window_with_keymap(content)
     local calculate_columns = function(columns)
-        local width = math.floor(columns * M.width)
+        local width = math.floor(columns * M.opts.width)
         return vim.o.columns - width
     end
 
     local opts = {
         relative = "editor",
-        width = math.floor(vim.o.columns * M.width),
-        height = math.floor(vim.o.lines * M.height),
+        width = math.floor(vim.o.columns * M.opts.width),
+        height = math.floor(vim.o.lines * M.opts.height),
         row = 0,
         col = calculate_columns(vim.o.columns),
         style = "minimal",
         border = "rounded",
     }
 
-    if M.float_buf and vim.api.nvim_buf_is_valid(M.float_buf) then
-        if M.float_win and vim.api.nvim_win_is_valid(M.float_win) then
-            vim.api.nvim_set_current_win(M.float_win)
-            return M.float_buf
+    if M.opts.float_buf and vim.api.nvim_buf_is_valid(M.opts.float_buf) then
+        if M.opts.float_win and vim.api.nvim_win_is_valid(M.opts.float_win) then
+            vim.api.nvim_set_current_win(M.opts.float_win)
+            return M.opts.float_buf
         else
-            M.float_win = vim.api.nvim_open_win(M.float_buf, true, opts)
-            return M.float_buf
+            M.opts.float_win = vim.api.nvim_open_win(M.opts.float_buf, true, opts)
+            return M.opts.float_buf
         end
     end
 
-    M.float_buf = vim.api.nvim_create_buf(false, true)
-    M.float_win = vim.api.nvim_open_win(M.float_buf, true, opts)
+    M.opts.float_buf = vim.api.nvim_create_buf(false, true)
+    M.opts.float_win = vim.api.nvim_open_win(M.opts.float_buf, true, opts)
 
-    vim.bo[M.float_buf].modifiable = true
-    vim.api.nvim_buf_set_lines(M.float_buf, 0, -1, false, content or { "No content" })
-    vim.bo[M.float_buf].modifiable = false
+    vim.bo[M.opts.float_buf].modifiable = true
+    vim.api.nvim_buf_set_lines(M.opts.float_buf, 0, -1, false, content or { "No content" })
+    vim.bo[M.opts.float_buf].modifiable = false
 
-    vim.bo[M.float_buf].readonly = true
-    vim.bo[M.float_buf].buftype = "nofile"
-    vim.bo[M.float_buf].bufhidden = "wipe"
-    vim.bo[M.float_buf].filetype = "jj"
+    vim.bo[M.opts.float_buf].readonly = true
+    vim.wo[M.opts.float_win].wrap = true;
+    vim.bo[M.opts.float_buf].buftype = "nofile"
+    vim.bo[M.opts.float_buf].bufhidden = "wipe"
+    vim.bo[M.opts.float_buf].filetype = "jj"
 
     vim.keymap.set("n", "q", function()
-        if M.float_win and vim.api.nvim_win_is_valid(M.float_win) then
-            vim.api.nvim_win_close(M.float_win, true)
+        if M.opts.float_win and vim.api.nvim_win_is_valid(M.opts.float_win) then
+            vim.api.nvim_win_close(M.opts.float_win, true)
         end
-    end, { buffer = M.float_buf, nowait = true, silent = true })
+    end, { buffer = M.opts.float_buf, nowait = true, silent = true })
 
-    return M.float_buf
+    return M.opts.float_buf
 end
 
 function M.set_content(buf, content)
@@ -207,8 +210,6 @@ end
 vim.api.nvim_create_user_command("JJ", function(args)
     if args.args == "status" or args.args == "st" then
         M.jj_st()
-    elseif args.args == "log" then
-        M.pick_log()
     elseif args.args == "describe" then
         M.jj_describe()
     elseif args.args == "branch-set" then
@@ -224,8 +225,33 @@ end, {
     desc = "JJ",
     nargs = "*",
     complete = function()
-        return { "commit-and-push", "push", "status", "st", "log", "describe", "branch-set" }
+        return { "commit-and-push", "push", "status", "st", "describe", "branch-set" }
     end,
 })
+
+function M.setup(opts)
+    M.opts = opts or M.opts
+    if M.opts.set_default_keymaps then
+        local set = vim.keymap.set
+        set("n", "<leader>jjl", function()
+            M.jj_graph()
+        end, { nowait = true, silent = true, desc = "JJ log" })
+        set("n", "<leader>jjs", function()
+            M.jj_st()
+        end, { nowait = true, silent = true, desc = "JJ status" })
+        set("n", "<leader>jjd", function()
+            M.jj_describe()
+        end, { nowait = true, silent = true, desc = "JJ describe" })
+        set("n", "<leader>jjb", function()
+            M.set_branch()
+        end, { nowait = true, silent = true, desc = "JJ branch" })
+        set("n", "<leader>jjp", function()
+            M.push()
+        end, { nowait = true, silent = true, desc = "JJ push" })
+        set("n", "<leader>jjc", function()
+            M.commit_and_push()
+        end, { nowait = true, silent = true, desc = "JJ commit and push" })
+    end
+end
 
 return M
